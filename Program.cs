4a70c219040d05a38be2b64dce1d2e14bcbe9bd0
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-10 16:49:49 +0530

    done

--- Program.cs
+++ Program.cs
@@ -8,10 +8,28 @@ namespace AspNetCore6.MinimalApi.Study
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
+            //  Please refer to: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/index/_static/request-delegate-pipeline.png?view=aspnetcore-7.0
+
+            //  Non-terminal middleware use Use()
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("MW1: Pre Next");
+                await next.Invoke();
+                Console.WriteLine("MW1: Post Next");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("MW2: Pre Next");
+                await next.Invoke();
+                Console.WriteLine("MW2: Post Next");
+            });
+
             //  Terminal middleware use Run()
             app.Run(async context =>
             {
                 //  context is the HttpContext, which houses both Request and Response
+                Console.WriteLine("Terminal MW");
                 await context.Response.WriteAsync("Hello, world!");
             });
 
