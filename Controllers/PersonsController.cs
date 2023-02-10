48efa1c438323a35c16cf332e4173041a79c8819
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-10 17:38:50 +0530

    done

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -3,20 +3,37 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore6.MinimalApi.Study.Controllers
 {
-    //  This is the controller request path
-    //  It's a good practice to version the APIs
-    //  in the route template, [controller] means
-    //  the name of the controller except Controller suffix (persons in the case)
     [Route("api/v1/[controller]")]
-    [ApiController] //  This provides automatic serialization of objects as Json
+    [ApiController]
     public class PersonsController : ControllerBase
     {
-        //  controller has action methods
-        [Route("foo")]  //  additional path for the action method (api/v1/persons/foo)
-        [HttpGet]   //  Responds for HttpGet
+        //  Route template can be provided with method
+        [HttpGet("foo")]
         public IActionResult Get()
         {
             return Ok(new { Name = "John Doe" });
         }
+
+        //  route template with int constraint
+        [HttpGet("by-id/{id:int}")]
+        public IActionResult Get(int id)    //  parameter should be called id
+        {
+            return Ok(new { Name = "John Doe", Id = id });
+        }
+
+        //  route template with string constraint (default)
+        [HttpGet("by-name/{name}")]
+        public IActionResult Get(string name)    //  parameter should be called id
+        {
+            return Ok(new { Name = name, Id = 100 });
+        }
+
+        //  Route constraints: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/routing?view=aspnetcore-7.0#route-constraints
+        
+        //  Warning: Don't use constraints for input validation. If constraints are
+        //  used for input validation, invalid input results in a 404 Not Found response.
+        //  Invalid input should produce a 400 Bad Request with an appropriate error message.
+        //  Route constraints are used to disambiguate similar routes, not to validate the
+        //  inputs for a particular route.
     }
 }
