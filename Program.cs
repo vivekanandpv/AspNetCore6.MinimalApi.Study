45f57c988a31fb080083c731c486fc6a29eda898
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-10 17:07:15 +0530

    done

--- Program.cs
+++ Program.cs
@@ -6,9 +6,27 @@ namespace AspNetCore6.MinimalApi.Study
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            //  builder is created with defaults
+            //  One such default is the ability access appsettings.json
+            //  This file is external to the Asp.Net Core system
+            //  But is represented by IConfiguration
+
+            //  To fetch simple values such as string, int, double, boolean, etc (not complex objects or arrays)
+            var allowedHosts = builder.Configuration.GetValue<string>("AllowedHosts");
+
+            //  To drill-down
+            var defaultLogLevel = builder.Configuration.GetValue<string>("Logging:LogLevel:Default");
+
+            //  For complex values (arrays and objects)
+            var cities = builder.Configuration.GetSection("Cities").Get<string[]>();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello World >> {allowedHosts} >> {defaultLogLevel}{Environment.NewLine}");
+                await context.Response.WriteAsync(string.Join(";", cities));
+            });
 
             app.Run();
         }
